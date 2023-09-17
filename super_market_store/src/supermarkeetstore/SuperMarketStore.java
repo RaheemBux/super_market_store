e2e5867958036cd9c0e26808ba30a18a045c29c0
Rahim Bux <raheembux991@gmail.com>
2023-09-17 17:42:03 +0500

    SMS-000003 : added sop

--- super_market_store/src/supermarkeetstore/SuperMarketStore.java
+++ super_market_store/src/supermarkeetstore/SuperMarketStore.java
@@ -16,6 +16,7 @@ public class SuperMarketStore {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("I have done some changes");
     }
     
 }
