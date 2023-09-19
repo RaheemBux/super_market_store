74d9ec4016ad9913b2871b3fe4fa1e045ded38ce
Govind Kumar <govindkk152@gmail.com>
2023-09-20 00:43:47 +0500

    SMS-CODE-REFACTOR -- incorporated changes

---
+++ super_market_store/src/constants/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package constants;
+
+/**
+ *
+ * @author Govind Kumar
+ */
+public enum Test {
+    
+}
